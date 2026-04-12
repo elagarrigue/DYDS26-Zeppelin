850cf36e06fada00f167f88dab865a13243d240d
Agustín Paul <a.agustinpaul@gmail.com>
2026-04-11 21:51:18 -0300

    refactor(presentation): Move ViewModel and bind to uses cases

---
+++ composeApp/src/desktopMain/kotlin/edu/dyds/movies/presentation/MoviesViewModel.kt
@@ -0,0 +1,61 @@
+package edu.dyds.movies.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import edu.dyds.movies.domain.entity.Movie
+import edu.dyds.movies.domain.entity.QualifiedMovie
+import edu.dyds.movies.domain.usecase.GetMovieDetailsUseCase
+import edu.dyds.movies.domain.usecase.GetPopularMoviesUseCase
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+
+class MoviesViewModel(
+    private val getPopularMoviesUseCase: GetPopularMoviesUseCase,
+    private val getMovieDetailsUseCase: GetMovieDetailsUseCase
+) : ViewModel() {
+
+    private val moviesStateMutableStateFlow = MutableStateFlow(MoviesUiState())
+    private val movieDetailStateMutableStateFlow = MutableStateFlow(MovieDetailUiState())
+
+    val moviesStateFlow: Flow<MoviesUiState> = moviesStateMutableStateFlow
+    val movieDetailStateFlow: Flow<MovieDetailUiState> = movieDetailStateMutableStateFlow
+
+    fun getAllMovies() {
+        viewModelScope.launch {
+            moviesStateMutableStateFlow.emit(
+                MoviesUiState(isLoading = true)
+            )
+            moviesStateMutableStateFlow.emit(
+                MoviesUiState(
+                    isLoading = false,
+                    movies = getPopularMoviesUseCase()
+                )
+            )
+        }
+    }
+
+    fun getMovieDetail(id: Int) {
+        viewModelScope.launch {
+            movieDetailStateMutableStateFlow.emit(
+                MovieDetailUiState(isLoading = true)
+            )
+            movieDetailStateMutableStateFlow.emit(
+                MovieDetailUiState(
+                    isLoading = false,
+                    movie = getMovieDetailsUseCase(id)
+                )
+            )
+        }
+    }
+
+    data class MoviesUiState(
+        val isLoading: Boolean = false,
+        val movies: List<QualifiedMovie> = emptyList(),
+    )
+
+    data class MovieDetailUiState(
+        val isLoading: Boolean = false,
+        val movie: Movie? = null,
+    )
+}
