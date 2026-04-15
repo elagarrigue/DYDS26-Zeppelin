119877c0dda081cdb0b6ca96cf19fff65ef5fbdd
Joaquín Méndez <joquimendez@gmail.com>
2026-04-15 10:19:29 -0300

    refactor(presentation): extract movie ui state models

---
+++ composeApp/src/desktopMain/kotlin/edu/dyds/movies/presentation/state/MovieDetailUiState.kt
@@ -0,0 +1,9 @@
+package edu.dyds.movies.presentation.state
+
+import edu.dyds.movies.domain.entity.Movie
+
+data class MovieDetailUiState(
+    val isLoading: Boolean = false,
+    val movie: Movie? = null,
+)
+
