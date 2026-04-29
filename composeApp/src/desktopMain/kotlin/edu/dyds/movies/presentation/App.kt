fdf40bfd145ded6e8edea54864ff24b03a66f9bb
Joaquín Méndez <joquimendez@gmail.com>
2026-04-29 12:12:11 -0300

    refactor: move navigation to presentation root

--- composeApp/src/desktopMain/kotlin/edu/dyds/movies/presentation/App.kt
+++ composeApp/src/desktopMain/kotlin/edu/dyds/movies/presentation/App.kt
@@ -1,7 +1,6 @@
 package edu.dyds.movies.presentation
 
 import androidx.compose.runtime.Composable
-import edu.dyds.movies.presentation.navigation.Navigation
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
---
+++ composeApp/src/desktopMain/kotlin/edu/dyds/movies/presentation/Navigation.kt
@@ -0,0 +1,57 @@
+package edu.dyds.movies.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import edu.dyds.movies.di.MoviesDependencyInjector.getMovieDetailsViewModel
+import edu.dyds.movies.di.MoviesDependencyInjector.getPopularMoviesViewModel
+import edu.dyds.movies.presentation.detail.DetailRoute
+import edu.dyds.movies.presentation.home.HomeRoute
+
+private const val HOME = "home"
+private const val DETAIL = "detail"
+private const val MOVIE_ID = "movieId"
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = HOME) {
+        homeDestination(navController)
+        detailDestination(navController)
+    }
+}
+
+private fun NavGraphBuilder.homeDestination(
+    navController: NavHostController,
+) {
+    composable(HOME) {
+        HomeRoute(
+            viewModel = getPopularMoviesViewModel(),
+            onGoodMovieClick = {
+                navController.navigate("$DETAIL/${it.id}")
+            }
+        )
+    }
+}
+
+private fun NavGraphBuilder.detailDestination(
+    navController: NavHostController,
+) {
+    composable(
+        route = "$DETAIL/{$MOVIE_ID}",
+        arguments = listOf(navArgument(MOVIE_ID) { type = NavType.IntType })
+    ) { backstackEntry ->
+        val movieId = backstackEntry.arguments?.getInt(MOVIE_ID)
+
+        movieId?.let {
+            DetailRoute(viewModel = getMovieDetailsViewModel(), id = it, onBack = { navController.popBackStack() })
+        }
+    }
+}
+
